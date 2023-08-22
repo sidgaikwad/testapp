2548b882afe37fb6d69e45b3fafa80728afd59a0
sidgaikwad <gaikwadsiddharth039@gmail.com>
2023-08-22 15:55:42 +0530

    first commit from ubuntu

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <p className=' text-white text-4xl font-semibold'>
+          hello world
+        </p>
+      </div>
+    </main>
+  )
+}
